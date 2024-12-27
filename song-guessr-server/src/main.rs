9b97c0f1c0bff34a14e319b5ba453e155d595f41
Thang Pham <phamducthang1234@gmail.com>
2024-12-26 21:52:35 -0500

    front-end plays sound

--- song-guessr-server/src/main.rs
+++ song-guessr-server/src/main.rs
@@ -1,10 +1,15 @@
+use anyhow::Context;
+use axum::{
+    http::{header::CONTENT_TYPE, HeaderValue, Method},
+    routing::get,
+    Router,
+};
 use rspotify::prelude::BaseClient;
+use tower_http::cors::CorsLayer;
 
 mod auth;
 
-#[tokio::main]
-
-async fn main() -> anyhow::Result<()> {
+async fn _get() -> anyhow::Result<String> {
     let cache =
         librespot_core::cache::Cache::new(Some(std::path::Path::new("/tmp")), None, None, None)?;
     let token = auth::get_token(cache).await?;
@@ -12,7 +17,24 @@ async fn main() -> anyhow::Result<()> {
 
     let id = rspotify::model::TrackId::from_id("1RMJOxR6GRPsBHL8qeC2ux")?;
     let track = client.track(id, None).await?;
-    println!("track: {track:?}");
+    Ok(track.preview_url.context("no preview url")?)
+}
+
+async fn get_api() -> Result<String, String> {
+    _get().await.map_err(|e| e.to_string())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers([CONTENT_TYPE]);
+
+    let app = Router::new().route("/get", get(get_api)).layer(cors);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentTrackUrl, setCurrentTrackUrl] = useState("");
+
+  useEffect(() => {
+    const fetchAudioUrl = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/get");
+        const text = await response.text();
+        setCurrentTrackUrl(text);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchAudioUrl();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Song Guesser</h1>
+      <audio src={currentTrackUrl} controls />
     </>
   );
 }
