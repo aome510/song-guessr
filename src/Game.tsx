5624f532dee6654e6632e450d5c71dbfc0cec018
Thang Pham <phamducthang1234@gmail.com>
2024-12-29 17:42:31 -0500

    cleanup Game page

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Question } from "./model.tsx";
 import { makeApiRequest } from "./utils.tsx";
+import { useNavigate } from "react-router-dom";
 
 function Game() {
-  const [selectedChoice, setSelectedChoice] = useState<number>(0);
   const [questionId, setQuestionId] = useState<number>(-1);
   const [questions, setQuestions] = useState<Array<Question>>([]);
-  const [audioSrc, setAudioSrc] = useState<string>("");
+
   const queryParams = new URLSearchParams(window.location.search);
   const playlistId = queryParams.get("playlist_id");
   const numQuestions = queryParams.get("num_questions") || "15";
+
+  const audio = useMemo(() => new Audio(), []);
+
+  useEffect(() => {
+    audio.autoplay = true;
+    audio.volume = 0.5;
+    return () => {
+      audio.pause();
+    };
+  }, [audio]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -25,7 +37,7 @@ function Game() {
           );
           setQuestions(data);
           setQuestionId(0);
-          setAudioSrc(data[0].choices[data[0].ans_id].preview_url);
+          audio.src = data[0].choices[data[0].ans_id].preview_url;
         } catch (err) {
           console.error(err);
         }
@@ -33,10 +45,9 @@ function Game() {
     };
 
     fetchQuestions();
-  }, [numQuestions, playlistId]);
+  }, [audio, numQuestions, playlistId]);
 
-  const handleChoiceSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  const handleChoiceSubmit = (selectedChoice: number) => {
     const question = questions[questionId];
     if (selectedChoice === question.ans_id) {
       alert("Correct!");
@@ -49,11 +60,11 @@ function Game() {
     // Move to the next question
     if (questionId + 1 < questions.length) {
       setQuestionId(questionId + 1);
-      setSelectedChoice(0); // Reset selected choice for the next question
       const next_question = questions[questionId + 1];
-      setAudioSrc(next_question.choices[next_question.ans_id].preview_url);
+      audio.src = next_question.choices[next_question.ans_id].preview_url;
     } else {
       alert("Quiz completed!");
+      navigate("/");
     }
   };
 
@@ -64,27 +75,21 @@ function Game() {
   return (
     <div>
       <h2>Question {questionId + 1}</h2>
-      <audio src={audioSrc} autoPlay controls />
-      <form onSubmit={handleChoiceSubmit}>
-        {questions[questionId].choices.map((choice, index) => (
-          <button
-            key={choice.name}
-            type="button"
-            onClick={() => setSelectedChoice(index)}
-            style={{
-              backgroundColor: selectedChoice === index ? "blue" : "gray",
-              color: "white",
-              margin: "5px",
-              padding: "10px",
-            }}
-          >
-            {choice.name}
-          </button>
-        ))}
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      {questions[questionId].choices.map((choice, index) => (
+        <button
+          key={choice.name}
+          type="button"
+          onClick={() => handleChoiceSubmit(index)}
+          style={{
+            backgroundColor: "gray",
+            color: "white",
+            margin: "5px",
+            padding: "10px",
+          }}
+        >
+          {choice.name}
+        </button>
+      ))}
     </div>
   );
 }
