de80e7d745def512588f55edb04c2e67bd090b6f
Thang Pham <phamducthang1234@gmail.com>
2025-01-01 15:48:32 -0500

    refactor client/api codes (#6)
    - move client logic into a new module: `client.rs`
    - move API logic into a new module: `api.rs`
    - use [`rspotify::AuthCodePkceSpotify`](https://docs.rs/rspotify/latest/rspotify/struct.AuthCodePkceSpotify.html) which supports token auto-fetching
    - remove `librespot` dependencies

--- song-guessr-server/src/main.rs
+++ song-guessr-server/src/main.rs
@@ -1,107 +1,13 @@
-use serde::{Deserialize, Serialize};
-use std::sync::Arc;
-use tokio::sync::RwLock;
-
-use axum::{
-    extract::{Path, Query, State},
-    http::{header::CONTENT_TYPE, HeaderValue, Method},
-    routing::get,
-    Json, Router,
-};
-use futures::stream::TryStreamExt;
-use futures_util::pin_mut;
-use rspotify::{
-    model::{FullTrack, PlayableItem, SearchType, SimplifiedPlaylist},
-    prelude::BaseClient,
-    AuthCodeSpotify,
-};
-use tower_http::cors::CorsLayer;
-
-mod auth;
+mod api;
+mod client;
 mod model;
-
-struct AppState {
-    client: AuthCodeSpotify,
-}
-
-type SharedState = Arc<RwLock<AppState>>;
-
-#[derive(Clone, Deserialize, Serialize)]
-struct QuestionQuery {
-    num_questions: Option<usize>,
-}
-
-async fn questions(
-    Path(playlist_id): Path<String>,
-    State(state): State<SharedState>,
-    Query(query): Query<QuestionQuery>,
-) -> Result<Json<Vec<model::Question>>, String> {
-    async fn api(
-        playlist_id: String,
-        n_questions: usize,
-        state: &AppState,
-    ) -> anyhow::Result<Vec<model::Question>> {
-        let playlist_id = rspotify::model::PlaylistId::from_id(playlist_id)?;
-        let stream = state.client.playlist_items(playlist_id, None, None);
-
-        let mut tracks: Vec<FullTrack> = Vec::new();
-        pin_mut!(stream);
-        while let Some(item) = stream.try_next().await? {
-            let track = item.track.unwrap();
-            if let PlayableItem::Track(track) = track {
-                tracks.push(track);
-            }
-        }
-        Ok(model::get_questions(tracks, n_questions))
-    }
-
-    let state = state.read().await;
-    let n_questions = query.num_questions.unwrap_or(15);
-    api(playlist_id, n_questions, &state)
-        .await
-        .map_err(|e| e.to_string())
-        .map(Json)
-}
-
-async fn playlist_search(
-    Path(query): Path<String>,
-    State(state): State<SharedState>,
-) -> Result<Json<Vec<SimplifiedPlaylist>>, String> {
-    async fn api(query: String, state: &AppState) -> anyhow::Result<Vec<SimplifiedPlaylist>> {
-        let result = state
-            .client
-            .search(&query, SearchType::Playlist, None, None, None, None)
-            .await?;
-        match result {
-            rspotify::model::SearchResult::Playlists(page) => Ok(page.items),
-            _ => Err(anyhow::anyhow!("invalid search result")),
-        }
-    }
-
-    let state = state.read().await;
-    api(query, &state)
-        .await
-        .map_err(|e| e.to_string())
-        .map(Json)
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let cache =
-        librespot_core::cache::Cache::new(Some(std::path::Path::new("/tmp")), None, None, None)?;
-    let token = auth::get_token(cache).await?;
-    let client = rspotify::AuthCodeSpotify::from_token(token);
-    let state = Arc::new(RwLock::new(AppState { client }));
+    let mut client = client::Client::new();
+    client.get_token().await?;
 
-    let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
-        .allow_headers([CONTENT_TYPE]);
-    let app = Router::new()
-        .route("/questions/:id", get(questions))
-        .route("/search/:query", get(playlist_search))
-        .layer(cors)
-        .with_state(state);
+    let app = api::new_app(client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
--- song-guessr-server/src/model.rs
+++ song-guessr-server/src/model.rs
@@ -1,8 +1,6 @@
-use rand::{seq::SliceRandom, thread_rng, Rng};
 use rspotify::model::FullTrack;
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Choice {
@@ -39,55 +37,17 @@ impl Ord for Choice {
     }
 }
 
-fn get_choice(track: FullTrack) -> Choice {
-    Choice {
-        name: track.name,
-        artist: track
-            .artists
-            .into_iter()
-            .map(|a| a.name)
-            .fold(String::new(), |s, a| s + &a + ", "),
-        preview_url: track.preview_url.unwrap(),
-        weight: track.popularity as i64,
-    }
-}
-
-pub fn get_questions(tracks: Vec<FullTrack>, n_questions: usize) -> Vec<Question> {
-    let mut questions: Vec<Question> = Vec::new();
-    let mut heap: BinaryHeap<Choice> = BinaryHeap::new();
-    let mut rng = thread_rng();
-
-    for track in tracks {
-        heap.push(get_choice(track));
-    }
-
-    for _ in 0..n_questions {
-        let mut top_choices: Vec<Choice> = Vec::new();
-
-        for _ in 0..4 {
-            if let Some(choice) = heap.pop() {
-                top_choices.push(choice);
-            }
-        }
-
-        top_choices.shuffle(&mut rng);
-
-        let ans_id = rng.gen_range(0..4);
-        let question = Question {
-            choices: top_choices.clone(),
-            ans_id,
-        };
-        questions.push(question);
-
-        for (index, mut choice) in top_choices.into_iter().enumerate() {
-            if index != ans_id {
-                choice.weight -= rng.gen_range(5..10);
-            } else {
-                choice.weight -= 18;
-            }
-            heap.push(choice);
+impl From<FullTrack> for Choice {
+    fn from(track: FullTrack) -> Self {
+        Self {
+            name: track.name,
+            artist: track
+                .artists
+                .into_iter()
+                .map(|a| a.name)
+                .fold(String::new(), |s, a| s + &a + ", "),
+            preview_url: track.preview_url.unwrap(),
+            weight: track.popularity as i64,
         }
     }
-
-    questions
 }
